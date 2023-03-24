951a643ff93340dbf6f81117bb807c5d771538f9
USsefRashed <USsefRashed@gmail.com>
2023-03-24 16:44:16 -0400

    Represent Noisy

--- Task.py
+++ Task.py
@@ -1,28 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as pt
+#In This is branch I will add noisy data in my data set and represent it graphically 
 
-#This is the first task of knowledge discovery subject
-
-# In this task I work on data for students grades in
-# maths exam to perform the data preprocessing and display the noisy ...etc 
-
+at=['age','G1']
 
 #Reading dataset File 
 dataset=pd.read_csv("./student-mat.csv")
 
-#Get first 5 objects of the dataset
-print(dataset.head())
-
-#Get the count of the rows and attributes
-print(dataset.shape)
-
-#Get the data types of columns
-print(dataset.dtypes)
-
-# Get the information data set
-print(dataset.info())
-
-# Get the information data set
-print(dataset.describe())
-
-# to see duplicated data I added it manually in branch with name Dublicated-Dealing
+dataset[at].boxplot()
+pt.title("Data")
+pt.show()
 
